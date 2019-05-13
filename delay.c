0c58ac0de890ab9cb222c930c22b21c225942af0
markyous <36309094+markyous@users.noreply.github.com>
2019-05-13 19:51:58 +0200

    Delay ver 1
    Co-Authored-By: omar mohamed alam <omaralam96@users.noreply.github.com>

---
+++ delay.c
@@ -0,0 +1,13 @@
+void delay_milliSecand(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3180;j++){
+        }
+    }
+}
+
+void delay_microSecand(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+        }
+    }
+}
---
+++ delay.h
@@ -0,0 +1,7 @@
+
+#ifndef delays
+#define delays
+/*...............function_definitions.............*/
+void delay_milliSecand(int n);
+void delay_microSecand(int n);
+#endif
