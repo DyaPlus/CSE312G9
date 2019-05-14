2c2a0b6031986d5280b8c2ab42e9fbeea88b3b01
markyous <36309094+markyous@users.noreply.github.com>
2019-05-14 23:01:05 +0200

    Create std_type.h

---
+++ std_type.h
@@ -0,0 +1,21 @@
+
+#ifndef STD_TYPE_H_
+#define STD_TYPE_H_
+
+typedef unsigned char         boolean;
+         /*           0 .. 255             */
+typedef signed char           sint8;          /*        -128 .. +127            */
+         /*           0 .. 65535           */
+typedef signed short          sint16;         /*      -32768 .. +32767          */
+typedef unsigned long         uint32;         /*           0 .. 4294967295      */
+typedef signed long           sint32;         /* -2147483648 .. +2147483647     */
+typedef unsigned long long    uint64;         /*       0..18446744073709551615  */
+typedef signed long long      sint64;
+typedef float                 float32;
+typedef double                float64;
+typedef unsigned char         pins_level;
+typedef unsigned char         uint8_t;
+typedef unsigned short        uint16_t;
+
+
+#endif /* STD_TYPE_H_ */
